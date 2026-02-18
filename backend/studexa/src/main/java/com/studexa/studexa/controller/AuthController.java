a96c444b53e4192e539c1a1485cbd5dc8d5b38ff
Aadam Ali <aadamali708@gmail.com>
2026-02-18 11:46:37 +0000

    now auth controller returns a response entity rather than userDtos or AuthResponses as this is for more security and compatability when requesting to these auth endpoints via the frontend and allows to catch errors such as invalid email and preventing same email being stored in the db before any queries are executed

--- backend/studexa/src/main/java/com/studexa/studexa/controller/AuthController.java
+++ backend/studexa/src/main/java/com/studexa/studexa/controller/AuthController.java
@@ -1,12 +1,18 @@
 package com.studexa.studexa.controller;
 
+import java.util.Map;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
 
-import com.studexa.studexa.dto.AuthResponse;
 import com.studexa.studexa.dto.LoginRequest;
 import com.studexa.studexa.dto.UserDto;
 import com.studexa.studexa.entity.User;
@@ -36,18 +42,58 @@ public class AuthController {
         }
 
     @PostMapping("/register")
-    public UserDto register(@RequestBody User user) {
+    public ResponseEntity<?> register(@RequestBody User user) {
 
-        return this.userService.register(user);
+        try{
+            UserDto dto = this.userService.register(user);
+            return ResponseEntity.ok(dto);
+
+        }catch(IllegalArgumentException e){
+            return ResponseEntity.status(400).body(Map.of("message", e.getMessage()));
+
+        }catch(DataIntegrityViolationException e){
+            return ResponseEntity.status(409).body(Map.of("message", "Email already Exists"));
+
+        }catch (Exception e){
+            return ResponseEntity.status(500).body(Map.of("message", "Error Registration failed"));
+        }
     }
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody LoginRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-        );
-        String token = jwtUtil.generateToken(request.getEmail());
-        return new AuthResponse(token);
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+
+
+        //for more security using spring security authentication manager
+        try{
+            authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+            );
+
+            //generating a jwt with the email as its username
+            String token = jwtUtil.generateToken(request.getEmail());
+
+            //returning a http cookie which stores the jwt (for local this is fine but later needs to be https)
+            ResponseCookie cookie = ResponseCookie.from("token", token)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(60*60)
+                .sameSite("None")
+                .build();
+
+            return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(Map.of("message", "Authenticated"));
+
+                //if a user doesnt exist then it will throw this bad credentials 401 response
+        }catch (BadCredentialsException ex) {
+            return ResponseEntity.status(401).body(Map.of("message", "Invalid credentials"));
+
+            //for an internal server error
+        } catch (Exception ex) {
+            return ResponseEntity.status(500).body(Map.of("message", "Login failed"));
+        }
+
     }
 
 
